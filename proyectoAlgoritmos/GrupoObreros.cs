3c2bb2d7179e417845b510b953229cc010f2e51f
Matias <matiasprofe2021@gmail.com>
2025-05-02 22:01:28 -0300

    borre comentarios por defecto

--- proyectoAlgoritmos/GrupoObreros.cs
+++ proyectoAlgoritmos/GrupoObreros.cs
@@ -1,11 +1,4 @@
-﻿/*
- * Created by SharpDevelop.
- * User: mati1
- * Date: 2/5/2025
- * Time: 21:02
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+﻿
 using System;
 
 namespace proyectoAlgoritmos
