64a1fa57bb7acd8bc0f5622af2161ad5f24bf0a4
Matias <matiasprofe2021@gmail.com>
2025-05-05 14:12:49 -0300

    Agrego herencia y constructor

--- proyectoAlgoritmos/JefedeObra.cs
+++ proyectoAlgoritmos/JefedeObra.cs
@@ -2,15 +2,25 @@
 
 namespace proyectoAlgoritmos
 {
-	/// <summary>
-	/// Description of JefedeObra.
-	/// </summary>
+
 	public class JefedeObra : Obrero
 	{
-		double bonificacion_Especial;
-		public JefedeObra(double bon_esp) : base(nom,  ape,  dni,  legajo,  sueldo,  cargo)
+		//Atributos
+		private double bonificacion_Especial;
+		
+		//Construtor
+		public JefedeObra(string nom, string ape, double dni, double legajo, double sueldo, string cargo, double bon_esp) : base(nom,  ape,  dni,  legajo,  sueldo,  cargo)
 		{
 			this.bonificacion_Especial = bon_esp;
+		}
+		
+		//Metodos
+		
+		public double getBonificacion(){
+			return this.bonificacion_Especial;
+		}
+		public void setBonificacion(double b){
+			this.bonificacion_Especial = b;
 		}
 	}
 }
